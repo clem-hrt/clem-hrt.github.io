98301d8d4139159f0399038d9aa11dcf6a78cc82
Clement Heritier <clem.heritier@laposte.net>
2026-06-30 11:41:35 -0400

    Update boot.js

--- js/boot.js
+++ js/boot.js
@@ -168,6 +168,7 @@ const Boot = (() => {
         CPU.create();
 
         SystemMonitor.create();
+        Ports.create();
         SystemMonitor.setPowerOnline();
         SystemMonitor.setCoreStatus("STANDBY");
         SystemMonitor.setModules(0, Network.getTotalModules());
