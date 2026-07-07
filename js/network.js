9e5faa1456d2a47129dac30ef0e4f068a913071a
Clement Heritier <clem.heritier@laposte.net>
2026-07-07 10:21:18 -0400

    Update network.js

--- js/network.js
+++ js/network.js
@@ -282,8 +282,8 @@ const Network = (() => {
             cpuSide: "bottom",
             moduleSide: "right",
             cpuSlot: 0.40,
-            busOffset: 400,
-            moduleStub: 100
+            busOffset: 1000,
+            moduleStub: 300
         },
 
         projects: {
@@ -304,8 +304,8 @@ const Network = (() => {
             cpuSide: "bottom",
             moduleSide: "left",
             cpuSlot: 0.60,
-            busOffset: 450,
-            moduleStub: 100
+            busOffset: 1050,
+            moduleStub: 300
         }
     };
     
